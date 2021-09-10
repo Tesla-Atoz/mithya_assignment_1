02d2b2b620e630c55c54c9ab1f0c51d523377787
Tesla-Atoz <teslaatozinspires7@gmail.com>
2021-09-10 12:43:24 +0530

    finished feature - render the form data as list.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import NewDetail from "./components/NewDetail/NewDetail";
+import DetailCard from "./components/DetailCard/DetailCard";
+import "./app.css";
+import { useState } from "react";
 
 function App() {
+  const UserData = [];
+
+  const [userDataFull, setUserDataFull] = useState(UserData);
+
+  const addDetailHandler = (detailData) => {
+    setUserDataFull((prevData) => [detailData, ...prevData]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NewDetail onAddDetail={addDetailHandler} />
+      <div className="app__item">
+        {userDataFull.map((data) => (
+          <DetailCard name={data.name} age={data.age} />
+        ))}
+      </div>
     </div>
   );
 }
